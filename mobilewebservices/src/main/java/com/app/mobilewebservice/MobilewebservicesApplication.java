d628262beb3c4b9ed2b6f641f71c254f253cf9e4
adidesi <adidesi95@gmail.com>
2018-08-27 17:10:02 +0530

    Solved the Tomcat 8 Deployment Issue

--- mobilewebservices/src/main/java/com/app/mobilewebservice/MobilewebservicesApplication.java
+++ mobilewebservices/src/main/java/com/app/mobilewebservice/MobilewebservicesApplication.java
@@ -2,11 +2,12 @@ package com.app.mobilewebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
-public class MobilewebservicesApplication {
+public class MobilewebservicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobilewebservicesApplication.class, args);
